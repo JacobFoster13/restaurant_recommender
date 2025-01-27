a769e5b93d67242b1783eb63dc7437378eebfe61
jacob13 <jacob13foster@outlook.com>
2025-01-26 19:07:55 -0600

    removed comments from setup file

--- setup.sh
+++ setup.sh
@@ -7,13 +7,3 @@ backgroundColor = \"#233041\"
 [server]
 headless = true
 enableCORS = false" > .streamlit/config.toml
-
-# [theme]
-# base = "dark"
-# primaryColor = "#4bd0ff"
-# backgroundColor = "#233041"
-
-# [server]
-# headless = true
-# enableCORS = false
-# port = $PORT
