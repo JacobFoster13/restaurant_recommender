80c3bfe182107fc51d0f84097137a1f4e8c29b09
Jacob Foster <jacob@Mac.attlocal.net>
2025-07-17 00:48:30 -0500

    added custom words to nltk sstopwords

--- utils.py
+++ utils.py
@@ -6,12 +6,13 @@ import string
 download("stopwords", force=True)
 download("punkt_tab", force=True)
 words_to_remove = set(stopwords.words("english"))
+[words_to_remove.add(word) for word in ['i', 'want', 'eat']]
 
 def generate_title(message):
     words = word_tokenize(message)
     filtered = []
     for word in words:
-        if ((word not in words_to_remove) and (word not in string.punctuation)):
+        if ((word.lower() not in words_to_remove) and (word not in string.punctuation)):
             filtered.append(word)
     
     return ' '.join(filtered[:5]).title() or "Untitled Chat"
